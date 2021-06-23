51f0ac2b97e2993989727f190f757e0dea2bb910
Filippo Valle <filippo.valle@to.infn.it>
2021-06-23 23:59:32 +0200

    fix deg_corr

--- sbmtm.py
+++ sbmtm.py
@@ -255,31 +255,25 @@ class sbmtm():
             if "count" in g.ep:
                 state_args["eweight"] = g.ep.count
 
+            
+            state_args["deg_corr"]=True
+            state_args["overlap"]=overlap
+
+            if B_max is None:
+                B_max = self.g.num_vertices()
+
             ## the inference
             mdl = np.inf  ##
             for i_n_init in range(n_init):
                 state_tmp = gt.minimize_nested_blockmodel_dl(g,
-                                                             state_args=dict(
-                                                                 deg_corr=True),
-                                                             overlap=overlap,
                                                              state_args=state_args,
-                                                             mcmc_args={'sequential': sequential},
-                                                             mcmc_equilibrate_args={
-                                                                 'mcmc_args': {'sequential': sequential}},
-                                                             mcmc_multilevel_args={
-                                                                 'mcmc_equilibrate_args': {
-                                                                     'mcmc_args': {'sequential': sequential}
-                                                                 },
-                                                                 'anneal_args': {
-                                                                     'mcmc_equilibrate_args': {
-                                                                         'mcmc_args': {'sequential': sequential}
-                                                                     }
-                                                                 }
+                                                             multilevel_mcmc_args={
+                                                                 "B_min":B_min,
+                                                                 "B_max":B_max,
+                                                                 "verbose":verbose
                                                              },
-                                                             B_min=B_min,
-                                                             B_max=B_max,
-                                                             verbose=verbose
                                                              )
+
                 mdl_tmp = state_tmp.entropy()
                 if mdl_tmp < mdl:
                     mdl = 1.0 * mdl_tmp
@@ -711,7 +705,7 @@ class sbmtm():
         counts = 'count' in self.g.ep.keys()
 
         ## count labeled half-edges, group-memberships
-        B = state_l.B
+        B = state_l.get_B()
         n_wb = np.zeros((V, B))  ## number of half-edges incident on word-node w and labeled as word-group tw
         n_db = np.zeros((D, B))  ## number of half-edges incident on document-node d and labeled as document-group td
         n_dbw = np.zeros((D, B))  ## number of half-edges incident on document-node d and labeled as word-group td
@@ -797,7 +791,7 @@ class sbmtm():
         state_l_edges = state_l.get_edge_blocks()  ## labeled half-edges
 
         ## count labeled half-edges, group-memberships
-        B = state_l.B
+        B = state_l.get_B()
         n_td_tw = np.zeros((B, B))
 
         counts = 'count' in self.g.ep.keys()
